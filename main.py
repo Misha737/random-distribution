1bb33a3ce1e8b88efab88ae1bbf569e0bd730bed
misha <makovkamisha10@gmail.com>
2024-12-20 15:24:29 +0200

    Improved the distribution of the graph axis

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ from distitribution import Distribution
 
 def chart(distribution: Distribution):
     fig, ax = mpl.subplots()
-    ax.bar(distribution.value, distribution.probably, color='#3452ad')
+    ticks_x = range(distribution.probably.size)
+    ax.bar(ticks_x, distribution.probably, color='#3452ad', align='center')
+
     ax.set_ylim(0, 1)
-    ticks = np.unique(np.concatenate((distribution.probably, np.linspace(0, 1, 5))))
-    ax.set_yticks(ticks)
+    # ticks_y = np.unique(np.concatenate((distribution.probably, np.linspace(0, 1, 5))))
+    ticks_y = np.linspace(0, 1, 9)
+    ax.set_yticks(ticks_y)
+
+    ax.set_xlim(-0.5, distribution.value.size - 0.5)
+    ax.set_xticks(ticks_x)
+    ax.set_xticklabels(distribution.value)
+
     ax.grid(which='major', linestyle='--', linewidth=0.5, color='gray')
     ax.set_title('Distribution')
     ax.set_ylabel('Probability')
